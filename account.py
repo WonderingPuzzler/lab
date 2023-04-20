a179141c476487ec6d468aefa1617a60ff9ea81f
WonderingPuzzler <whereiwander80@gmail.com>
2023-04-20 18:02:36 -0500

    Updated account.py

--- account.py
+++ account.py
@@ -1,8 +1,9 @@
 class Account:
-    def __init__(self,name):
+
+    def __init__(self, name):
         self.__account_name = name
         self.__balance = 0
-        
+
     def deposit(self, amount):
         if amount > 0:
             self.__balance += amount
@@ -10,8 +11,16 @@ class Account:
         else:
             return False
         
+
+    def withdraw(self, amount):
+        if amount > 0 and amount <= self.__balance:
+            self.__balance -= amount
+            return True
+        else:
+            return False
+
     def get_balance(self):
         return self.__balance
-    
+
     def get_name(self):
-        return self.__acount_name
+        return self.__account_name
